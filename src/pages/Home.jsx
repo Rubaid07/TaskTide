b30917ec97fe28b03d0caf682dc8096f830699cc
Rubaid07 <rubaidindu@gmail.com>
2025-05-21 16:39:28 +0600

    added platform section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Hero from './Hero';
 import FeaturedTask from './FeaturedTask';
-import TopCategories from './HowItWork';
+
+import Platform from './Platform';
+import HowItWorks from './HowItWork';
 
 const Home = () => {
     return (
         <div className='md:my-10 my-5'>
             <Hero></Hero>
             <FeaturedTask></FeaturedTask>
-            <TopCategories></TopCategories>
+            <HowItWorks></HowItWorks>
+            <Platform></Platform>
         </div>
     );
 };
