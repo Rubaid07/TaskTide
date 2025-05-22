b38b4d6917661240cb48eeb3df12208faa1dfc4d
Rubaid07 <rubaidindu@gmail.com>
2025-05-22 20:33:40 +0600

    moved firebase config to .env

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,17 +1,11 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAtR-2ynZZYee7cFqTGrK9ec9K4K6l8uD0",
-  authDomain: "tasktide07.firebaseapp.com",
-  projectId: "tasktide07",
-  storageBucket: "tasktide07.firebasestorage.app",
-  messagingSenderId: "345519399839",
-  appId: "1:345519399839:web:d88cedd18e98d5592a53aa"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
